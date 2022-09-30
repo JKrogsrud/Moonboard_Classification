b75d7f792e071f7580cfe995451b814342c7eeb8
NicholasGibsonUVM <nsgibson@uvm.edu>
2022-09-30 11:33:07 -0400

    Added Conversion Function

--- Helpers/gradeConversion.py
+++ Helpers/gradeConversion.py
@@ -1,5 +1,38 @@
 def font_to_v(grade):
-    pass
-
-def v_to_font(grade):
-    pass
+    if grade == "4":
+        return 0
+    elif grade == "5":
+        return 1
+    elif grade == "5+":
+        return 2
+    elif grade == "6a" or grade == "6a+":
+        return 3
+    elif grade == "6b" or grade == "6b+":
+        return 4
+    elif grade == "6c":
+        return 5
+    elif grade == "6c+" or grade == "7a":
+        return 6
+    elif grade == "7a+":
+        return 7
+    elif grade == "7b" or grade == "7b+":
+        return 8
+    elif grade == "7c":
+        return 9
+    elif grade == "7c+":
+        return 10
+    elif grade == "8a":
+        return 11
+    elif grade == "8a+":
+        return 12
+    elif grade == "8b":
+        return 13
+    elif grade == "8b+":
+        return 14
+    elif grade == "8c":
+        return 15
+    elif grade == "8c+":
+        return 16
+    elif grade == "9a":
+        return 17
+    return -1
