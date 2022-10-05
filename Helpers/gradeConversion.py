84fb692aca29242f375a5d71f0088705973905c6
NicholasGibsonUVM <nsgibson@uvm.edu>
2022-10-05 13:41:28 -0400

    Converted Font to V scale for grade

--- Helpers/gradeConversion.py
+++ Helpers/gradeConversion.py
@@ -5,34 +5,34 @@ def font_to_v(grade):
         return 1
     elif grade == "5+":
         return 2
-    elif grade == "6a" or grade == "6a+":
+    elif grade == "6A" or grade == "6A+":
         return 3
-    elif grade == "6b" or grade == "6b+":
+    elif grade == "6B" or grade == "6B+":
         return 4
-    elif grade == "6c":
+    elif grade == "6C":
         return 5
-    elif grade == "6c+" or grade == "7a":
+    elif grade == "6C+" or grade == "7A":
         return 6
-    elif grade == "7a+":
+    elif grade == "7A+":
         return 7
-    elif grade == "7b" or grade == "7b+":
+    elif grade == "7B" or grade == "7B+":
         return 8
-    elif grade == "7c":
+    elif grade == "7C":
         return 9
-    elif grade == "7c+":
+    elif grade == "7C+":
         return 10
-    elif grade == "8a":
+    elif grade == "8A":
         return 11
-    elif grade == "8a+":
+    elif grade == "8A+":
         return 12
-    elif grade == "8b":
+    elif grade == "8B":
         return 13
-    elif grade == "8b+":
+    elif grade == "8B+":
         return 14
-    elif grade == "8c":
+    elif grade == "8C":
         return 15
-    elif grade == "8c+":
+    elif grade == "8C+":
         return 16
-    elif grade == "9a":
+    elif grade == "9A":
         return 17
     return -1
